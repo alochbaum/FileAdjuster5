8b6688382385e2855a179ee40275db270483217b
Andrew Lochbaum <andrew.lochbaum@live.com>
2018-06-18 15:52:32 -0400

    Put program name an version in the window title

--- FileAdjuster5/MainWindow.xaml.cs
+++ FileAdjuster5/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,8 @@ namespace FileAdjuster5
         public MainWindow()
         {
             InitializeComponent();
+            // Adding the version number to the title
+            MainFrame.Title = "File Adjuster version:" + Assembly.GetExecutingAssembly().GetName().Version;
         }
     }
 }
