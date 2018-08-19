8e878e7f7fe30f1b53683a064312c2ba912cd8ea
Andrew Lochbaum <andrew.lochbaum@live.com>
2018-08-19 17:30:32 -0400

    Fixed the spacing in the Rich Text Box

--- FileAdjuster5/Properties/AssemblyInfo.cs
+++ FileAdjuster5/Properties/AssemblyInfo.cs
@@ -51,8 +51,8 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.1.11.0")]
-[assembly: AssemblyFileVersion("5.1.11.0")]
+[assembly: AssemblyVersion("5.1.12.12")]
+[assembly: AssemblyFileVersion("5.1.12.12")]
 
 // Added for logging
 [assembly: log4net.Config.XmlConfigurator(Watch = true)]
