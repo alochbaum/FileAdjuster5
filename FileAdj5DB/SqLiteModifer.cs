f5087e81b29ccfc4846ca11cc499c0caeb973efd
Andrew Lochbaum <andrew.lochbaum@live.com>
2019-07-02 22:58:22 -0400

    Started SqLite class for Adjuster

---
+++ FileAdj5DB/SqLiteModifer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using System.Threading.Tasks;
+
+namespace FileAdj5DB
+{
+    class SqLiteModifer
+    {
+    }
+}
