4cdacc4b606c0be57fd00508a6c5e8082d1cbf77
Andrew Lochbaum <andrew.lochbaum@live.com>
2019-08-04 22:48:36 -0400

    trying to add color to textbox with additional static class

---
+++ FileAdj5DB/RichTBEnhance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace FileAdj5DB
+{
+    static class RichTBEnhance
+    {
+        /// <summary>
+        /// Extends the rich text box with color
+        /// sample myRichTextBox.AppendText("My text", "0xffffff");
+        /// or myRichTextBox.AppendText("My text", "CornflowerBlue");
+        /// </summary>
+        /// <param name="box"></param>
+        /// <param name="text"></param>
+        /// <param name="color"></param>
+        public static void AppendText(this RichTextBox box, string text, string color)
+        {
+            BrushConverter bc = new BrushConverter();
+            TextRange tr = new TextRange(box.Document.ContentEnd, box.Document.ContentEnd);
+            tr.Text = text;
+            try
+            {
+                tr.ApplyPropertyValue(TextElement.ForegroundProperty,
+                    bc.ConvertFromString(color));
+            }
+            catch (FormatException) { }
+        }
+    }
+}
